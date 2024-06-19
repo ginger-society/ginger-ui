aeed513e79ae0647380a97d4a9060eb7e35755af
Ginger Society Admin <admin@gingersociety.org>
2024-06-19 21:09:52 +0530

    fix: theme fixes for checkbox

--- src/components/checkbox/checkbox.stories.tsx
+++ src/components/checkbox/checkbox.stories.tsx
@@ -4,17 +4,36 @@ import Checkbox from './Checkbox'
 
 export const ExampleCheckboxComponent: React.FC = () => {
 	const [isChecked, setIsChecked] = useState(false)
+	const [isWhatsappChecked, setIsWhatsappChecked] = useState(false)
+	const [is2FAChecked, setIs2FAChecked] = useState(false)
 
 	const handleChange = (checked: boolean) => {
 		setIsChecked(checked)
 	}
 
+	const handleWhatsappChange = (checked: boolean) => {
+		setIsWhatsappChecked(checked)
+	}
+	const handle2FAChange = (checked: boolean) => {
+		setIs2FAChecked(checked)
+	}
+
 	return (
-		<div>
+		<div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
 			<Checkbox
 				label="Enable notifications"
 				checked={isChecked}
 				onChange={handleChange}
+			/>
+			<Checkbox
+				label="Allow whatsapp notification"
+				checked={isWhatsappChecked}
+				onChange={handleWhatsappChange}
+			/>
+			<Checkbox
+				label="Enable 2FA"
+				checked={is2FAChecked}
+				onChange={handle2FAChange}
 			/>
 		</div>
 	)
