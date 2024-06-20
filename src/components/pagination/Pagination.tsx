61a4a7890eaf650d7f3800aeee90a93fa42e8a79
Ginger Society Admin <admin@gingersociety.org>
2024-06-20 20:44:31 +0530

    fix: lint fixes

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Button, ButtonType } from '../button'
 import { Dropdown } from '../dropdown'
 import styles from './pagination.module.scss'
