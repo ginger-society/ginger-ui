2fa73c07899ac512906eb14cafd61ee743b94eac
Ginger Society Admin <admin@gingersociety.org>
2024-07-16 15:34:10 +0530

    feat: added HandleLoginRedirect component , added react-router-dom as dependency

---
+++ src/components/handleLoginRedirect/index.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import LoadingPage from '../loadingPage'
+
+interface HandleAuthProps {
+	handleNavigation: () => void
+}
+
+const HandleLoginRedirect = ({ handleNavigation }: HandleAuthProps) => {
+	const { access_token, refresh_token } = useParams<{
+		access_token: string
+		refresh_token: string
+	}>()
+
+	useEffect(() => {
+		if (access_token && refresh_token) {
+			localStorage.setItem('access_token', access_token)
+			localStorage.setItem('refresh_token', refresh_token)
+
+			handleNavigation()
+		}
+	}, [access_token, refresh_token, handleNavigation])
+
+	return <LoadingPage />
+}
+
+export default HandleLoginRedirect
